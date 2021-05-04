9bfc54673fa6110749144537b18846ca20094ddf
Irving Henriquez <henriquezirv@gmail.com>
2021-05-04 16:23:38 +0000

    fixed typewriter bug

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,11 @@
 const sentence = "hello there from lighthouse labs";
-let count = 500
-for (const char of sentence) {
-  
-  setTimeout((count)=>{
-    
-    process.stdout.write(char + '\n') ;
-    
-  },count += 500) 
-  
+let count = 500;
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout((count) => {
+    if (i !== sentence.length - 1) {
+      process.stdout.write(sentence[i]);
+    } else {
+      process.stdout.write(sentence[i]+ "\n");
+    }
+  }, (count += 500));
 }
